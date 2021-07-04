95e030bd48388738c251790c446a592b5b52c326
Mohammed ALJazaeri <pc-moon@hotmail.com>
2021-07-04 14:44:28 +0300

    Install VueJs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+
+import Project from './Components/Project.vue';
+
+
+new Vue({
+    el: '#app',
+    components: {
+        'project': Project
+    }    
+})
